b816b8583baf3fab38d36df86f448294432eeb1a
gonzaemon111 <goki14914@icloud.com>
2018-09-02 02:08:05 +0900

    make articles_controller.rb

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show update destroy]
+  def index
+    @articles = Article.includes(:comment)
+    render json: @articles, status: 200
+  end
+
+  def show
+    render json: @article, status: 200
+  end
+
+  def create
+    @article = Article.new(arcticle_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @article.update
+      render json: @article, status: 200
+    else
+      render json: @article.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      render json: { id: params[:id].to_i }
+    else
+      render json: @article.errors.full_messages, status: 400
+    end
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def arcticle_params
+    params.require(:article).permit(
+      :title,
+      :content
+    )
+  end
+end
