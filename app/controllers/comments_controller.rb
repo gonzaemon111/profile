26169efcd497ce809c0beaa5248e4fb892c4d8f5
gonzaemon111 <goki14914@icloud.com>
2018-09-09 23:25:51 +0900

    add controller for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  before_action :set_article
+
+  def index
+    @comments = Comment.includes(:article)
+    render json: @comments, status: 200
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, status: 200
+    else
+      render json: @comment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_aricle
+    @article = Article.find(params[:article_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(
+      :name,
+      :comment
+    ).merge(
+      article_id: @article.id
+    )
+  end
+end
