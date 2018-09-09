5eda3e36dee0df73a91262157c26c34f73ce6d84
gonzaemon111 <goki14914@icloud.com>
2018-09-09 23:32:51 +0900

    add spec article and comment

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Article, type: :model do
       it { is_expected.to validate_presence_of(:title) }
       it { is_expected.to validate_presence_of(:content) }
     end
+
+    context "association" do
+      subject { create(:article) }
+      it { is_expected.to have_many(:comments) }
+    end
   end
 end
